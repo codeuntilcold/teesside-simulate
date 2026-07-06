7c73d8da98a63c49681fabc7e7508d0483d27eab
Dung Ngo Le Quoc <quocdungngole@gmail.com>
2026-07-06 17:06:37 +0700

    script to run noise experiments

--- run_k_sweep.sh
+++ run_k_sweep.sh
@@ -1,27 +1,35 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 go build -o teesside-go main.go
 
 t=$(( $(nproc) - 1 ))
 parallel_params="--halt now,fail=1 -j $t --eta --bar"
 
-common="--game-type pd --beta 1.8 --theta-start 0 --theta-end 10 --theta-step 0.1"
-k_values="0.1 0.2 0.3 0.5 1.0"
+k_values="0.1 0.3 0.5"
+pd="--game-type pd --beta 1.8 --theta-start 0 --theta-end 10 --theta-step 0.1"
+pgg="--game-type pgg --r 3.0 --theta-start 0 --theta-end 10 --theta-step 0.1"
 
-# POP, p_C = 1.0 -- one output dir per K (dir name must start with pop_ for preprocess)
-parallel $parallel_params ./teesside-go $common --strategy pop --pc 1.0 \
+# PD b=1.8
+parallel $parallel_params ./teesside-go $pd --strategy pop --pc 1.0 \
     --fermi-k {1} --seed-start {2} --seed-end '$(('{2}'+1))' \
-    --output-dir data_ksweep/pop_k{1} \
-    ::: $k_values \
-    ::: {0..49}
+    --output-dir 'data_knoise/pop_b=1.8_k{1}' \
+    ::: $k_values ::: {0..49}
+
+parallel $parallel_params ./teesside-go $pd --strategy neb --nc {3} \
+    --fermi-k {1} --seed-start {2} --seed-end '$(('{2}'+1))' \
+    --output-dir 'data_knoise/neb_b=1.8_k{1}' \
+    ::: $k_values ::: {0..49} ::: 3 4
+
+# PGG r=3.0
+parallel $parallel_params ./teesside-go $pgg --strategy pop --pc 1.0 \
+    --fermi-k {1} --seed-start {2} --seed-end '$(('{2}'+1))' \
+    --output-dir 'data_knoise/pop_pgg_r=3.0_k{1}' \
+    ::: $k_values ::: {0..49}
 
 # NEB, n_C = 3
-parallel $parallel_params ./teesside-go $common --strategy neb --nc 3 \
+parallel $parallel_params ./teesside-go $pgg --strategy neb --nc {3} \
     --fermi-k {1} --seed-start {2} --seed-end '$(('{2}'+1))' \
-    --output-dir data_ksweep/neb_k{1} \
-    ::: $k_values \
-    ::: {0..49}
-
-# for d in data_ksweep/*_k*; do
-#     python preprocess_seeds.py --data-dir "$d" --output-dir "${d/data_ksweep/data_ksweep_agg}"
-# done
+    --output-dir 'data_knoise/neb_pgg_r=3.0_k{1}' \
+    ::: $k_values ::: {0..49} ::: 3 4
